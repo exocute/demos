2d8b212827138af1a66b1e6130bc540c86c46929
Eduardo <eduardo.s.rodrigues@ist.utl.pt>
2017-01-26 15:26:55 +0000

    Picture to Picture8Bit targetName added

--- src/main/scala/VideoEncodeDecode.scala
+++ src/main/scala/VideoEncodeDecode.scala
@@ -2,9 +2,10 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-import org.jcodec.api.FrameGrab
+import it.sauronsoftware.jave._
+import org.jcodec.api.{FrameGrab, FrameGrab8Bit}
 import org.jcodec.api.awt.AWTSequenceEncoder
-import org.jcodec.common.model.Picture
+import org.jcodec.common.model.{Picture, Picture8Bit}
 import org.jcodec.scale.AWTUtil
 
 /**
@@ -12,29 +13,76 @@ import org.jcodec.scale.AWTUtil
   */
 object VideoEncodeDecode {
 
-  def produceImage(fileName: String, imageType: String, frames: Int) = {
+  def produceImage(fileName: String, targetName: String, imageType: String, frames: Int) = {
     println("Decoding Video...")
     val time: Long = System.currentTimeMillis()
     val file: File = new File(fileName)
-    for (i <- 0 to frames) {
-      val frame: Picture = FrameGrab.getFrameFromFile(file, i)
-      val framebuf: BufferedImage = AWTUtil.toBufferedImage(frame)
-      ImageIO.write(framebuf, imageType, new File("frames\\image" + i + "." + imageType))
+    for (i <- 0 until frames) {
+      val frame: Picture8Bit = FrameGrab8Bit.getFrameFromFile(file, i)
+      val framebuf: BufferedImage = AWTUtil.toBufferedImage8Bit(frame)
+      ImageIO.write(framebuf, imageType, new File(targetName + i + "." + imageType))
     }
+    //extractAudio(fileName, "audio\\audio.mp3")
     println("Decoded in:" + (System.currentTimeMillis() - time) + " Milliseconds")
   }
 
-  def produceVideo(fileName: String, frameRate: Int,, imageType: String, keyFrames: Vector[Boolean]) = {
+  def main(args: Array[String]): Unit = {
+    produceImage("la-la-land-trailer-4_h480p.mp4","frames\\image","jpg", 100)
+  }
+
+  def produceVideo(fileName: String, targetName: String, frameRate: Int, imageType: String, keyFrames: Vector[Boolean]) = {
+
+    def encodeImage(encoder: AWTSequenceEncoder, index: Int) = {
+      val image: BufferedImage = ImageIO.read(new File(targetName + index + "." + imageType))
+      encoder.encodeImage(image, keyFrames(index))
+    }
+
     println("Encoding Video...")
     val time: Long = System.currentTimeMillis()
     val enc: AWTSequenceEncoder = AWTSequenceEncoder.createSequenceEncoder(new File(fileName), frameRate)
 
-    for (i <- 0 to keyFrames.size) {
-      val image: BufferedImage = ImageIO.read(new File("frames\\image" + i + "." + imageType))
-      enc.encodeImage(image, keyFrames(i))
+    //first image should always be a keyframe
+    encodeImage(enc, 0)
+
+    for (index <- 1 until keyFrames.size) {
+      encodeImage(enc, index)
     }
     enc.finish()
+
+    /*val rt: Runtime = Runtime.getRuntime()
+    val pr: Process = rt.exec("ffmpeg -i " + fileName + " -i audio\\audio.mp3 -c:v copy -c:a aac -strict experimental compressed.mp4");
+    pr.waitFor()*/
     println("Encoded in:" + (System.currentTimeMillis() - time) + " Milliseconds")
   }
+
+  /*def extractAudio(sourceFileName: String, targetFileName: String) = {
+    val source: File = new File(sourceFileName)
+    val target: File = new File(targetFileName)
+    val audio: AudioAttributes = new AudioAttributes
+    audio.setCodec("libmp3lame")
+    audio.setBitRate(new Integer(128000))
+    audio.setChannels(new Integer(2))
+    audio.setSamplingRate(new Integer(44100))
+    val attrs: EncodingAttributes = new EncodingAttributes
+    attrs.setFormat("mp3")
+    attrs.setAudioAttributes(audio)
+    val encoder: Encoder = new Encoder
+    try
+      encoder.encode(source, target, attrs)
+    catch {
+      case e: IllegalArgumentException => {
+        // TODO Auto-generated catch block
+        e.printStackTrace()
+      }
+      case e: InputFormatException => {
+        // TODO Auto-generated catch block
+        e.printStackTrace()
+      }
+      case e: EncoderException => {
+        // TODO Auto-generated catch block
+        e.printStackTrace()
+      }
+    }
+  }*/
 
 }
